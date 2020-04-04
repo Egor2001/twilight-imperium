cffbedee074767eb77ea9a5eb1b01b189c680afa
aleks311001 <aleks311001@yandex.ru>
2020-04-04 18:07:55 +0300

    Add python generator JSON

--- src/base/Main.java
+++ src/base/Main.java
@@ -5,8 +5,13 @@ import ArmyUnits.Ships.Flagship;
 
 public class Main {
     public static void main(String[] args) {
-        GameController gameController = GameController.getInstance();
+
+        ShipFactoryRace1 F1 = new ShipFactoryRace1();
+        Flagship Flagship1 = F1.createFlagship();
+        System.out.print(Flagship1.getCost());
+
+        /*GameController gameController = GameController.getInstance();
         gameController.gameInit();
-        gameController.gameLoop();
+        gameController.gameLoop();*/
     }
 }
