1019fe409d5b39310b259a425fa5573a414af526
aleks311001 <aleks311001@yandex.ru>
2020-04-27 01:00:00 +0300

    pull

--- src/base/Fight/SpaceCombatController.java
+++ src/base/Fight/SpaceCombatController.java
@@ -1,37 +1,43 @@
 package base.Fight;
 
 import ArmyUnits.Unit;
+import tile.Tile;
+import tile.TileArmyController;
 
 import java.util.ArrayList;
 
 public class SpaceCombatController {
-    public SpaceCombatController() {
+    private boolean first_player_announce_retreat;
+    private boolean second_player_announce_retreat;
+    private Tile tileFight;
+    private TileArmyController tileArmyController;
+    private ArrayList<Unit> spaceUnit;
+
+    public SpaceCombatController(Tile tile, TileArmyController tileArmyController) {
         first_player_announce_retreat = false;
         second_player_announce_retreat = false;
+        tileFight = tile;
+        this.tileArmyController = tileArmyController;
+        units = tileArmyController.getUnit(tile.getSpace());
     }
 
-    public void anti_fighter_barrage(/*ArrayList<Unit> first_army_units, ArrayList<Unit> second_army_units*/)
-    {
-
-    }
-
-    public void make_combat_rolls()
-    {
+    public void anti_fighter_barrage(/*ArrayList<Unit> first_army_units, ArrayList<Unit> second_army_units*/) {
 
     }
 
-    public void assign_hits()
-    {
+    public void make_combat_rolls() {
 
     }
 
-    public void announce_retreat()
-    {
+    public void assign_hits() {
 
     }
 
-    public void retreat()
-    {
+    public void announce_retreat() {
+
+    }
+
+    public void retreat() {
 
     }
 
@@ -54,7 +60,4 @@ public class SpaceCombatController {
         }
 
     }
-
-    private boolean first_player_announce_retreat;
-    private boolean second_player_announce_retreat;
 }
