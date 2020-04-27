8458e2e9c4cacc918283c9e2e89da47062a0b50e
aleks311001 <aleks311001@yandex.ru>
2020-04-28 00:31:50 +0300

    fix units

--- PythonGenerateUnitsJSON/GenerateInfantry.py
+++ PythonGenerateUnitsJSON/GenerateInfantry.py
@@ -4,8 +4,8 @@ d = dict()
 
 race_names = {1: 'Arborec', 2: 'Naalu_Collective',
               3: 'Barony_of_Letnev', 4: 'Nekro_Virus',
-              5: 'Clan_of_Saar', 6: "Sardakk_N'orr",
-              7: 'Embers_of_Muaat', 8: 'Universities_of_Jol-Nar',
+              5: 'Clan_of_Saar', 6: "Sardakk_Norr",
+              7: 'Embers_of_Muaat', 8: 'Universities_of_Jol_Nar',
               9: 'Emirates_of_Hacan', 10: 'Winnu',
               11: 'Federation_of_Sol', 12: 'Xxcha_Kingdom',
               13: 'Ghosts_of_Creuss', 14: 'Yin_Brotherhood',
@@ -14,8 +14,12 @@ race_names = {1: 'Arborec', 2: 'Naalu_Collective',
 
 race_name = race_names[1]
 
-d['combatValue'] = 7
 d['cost'] = 1
+d['costNumUnits'] = 2
+
+d['combatValue'] = 7
+d['combatNumDices'] = 1
+
 d['canFightSpace'] = False
 
 for race_name in race_names.values():
