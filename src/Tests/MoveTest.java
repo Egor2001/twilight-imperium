0cbe8eef3d6c2bd3c9f7215655a4402fbaf819d4
IgorMelnikov1 <melnikov.ia@phystech.edu>
2020-04-28 00:29:03 +0300

    to merge with develop

--- src/Tests/MoveTest.java
+++ src/Tests/MoveTest.java
@@ -49,3 +49,42 @@ public class MoveTest extends TestCase {
         }
     }
 }
+
+class SimpleMove extends TestCase {
+    protected void setUp() throws Exception {
+
+    }
+
+    protected void tearDown() throws Exception {
+
+    }
+
+    public void testMove () {
+        try {
+            GameController gm = GameController.getInstance();
+            TileArmyController controller = gm.getGameState().getController();
+            Board board = gm.getGameState().getBoard();
+            ArrayList<Player> players = gm.getGameState().getPlayers();
+            players.add(new Player("Igor", "Arborec"));
+
+            controller.add(players.get(0).addUnit("Carrier"), board.tiles_.get(2).space_);
+            controller.add(players.get(0).addUnit("Carrier"), board.tiles_.get(1).space_);
+            controller.add(players.get(0).addUnit("Destroyer"), board.tiles_.get(1).space_);
+
+            //System.out.println(board.getView(null));
+
+            HierarchyController.GameObjectTarget tr = HierarchyController.parseTarget("Army.Ship.0");
+            ArrayList<HierarchyController.GameObjectTarget> list = new ArrayList<>();
+            list.add(HierarchyController.parseTarget("Tile.1.Space"));
+
+            gm.getGameState().handleActionCommand(players.get(0), new PhaseController.PlayerActionMove(tr, list));
+            //System.out.println(board.getView(null));
+
+            HierarchyController.GameObjectTarget tr2 = HierarchyController.parseTarget("Army.Ship.2");
+            //System.out.println(((Unit) players.get(0).getObject(tr2)).getInfo());
+        }
+        catch (Exception e) {
+            fail();
+        }
+    }
+}
