fb8c9f98b0f8ce9d91648c9a5f10927751c93a42
IgorMelnikov1 <melnikov.ia@phystech.edu>
2020-05-01 20:33:26 +0300

    move test

--- src/tests/MoveTest.java
+++ src/tests/MoveTest.java
@@ -13,6 +13,10 @@ import board.Board;
 import board.TileArmyManager;
 
 import java.io.*;
+import java.lang.invoke.StringConcatFactory;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class MoveTest extends Assert {
@@ -29,7 +33,17 @@ public class MoveTest extends Assert {
     @Test
     public void testMove () {
         try {
-            DoTestFromFile();
+            DoTestFromFile("test1");
+        }
+        catch (Exception e)
+        {
+            fail();
+        }
+    }
+    @Test
+    public void otherMove () {
+        try {
+            DoTestFromFile("test2");
         }
         catch (Exception e)
         {
@@ -42,14 +56,53 @@ public class MoveTest extends Assert {
 
     }
 
-    private String DoTestFromFile() throws FileNotFoundException {
-        File input_file = new File("tests.txt");
-        FileOutputStream fileOutputStream = new FileOutputStream("out_texst.txt", true);
+    private void DoTestFromFile(String test_name) throws FileNotFoundException {
+        try {
+            File input_file = new File("test/" + test_name + "/commands.txt");
 
+            FileWriter fstream1 = new FileWriter("out_text.txt");// конструктор с одним параметром - для перезаписи
+            BufferedWriter out1 = new BufferedWriter(fstream1); //  создаём буферезированный поток
+            out1.write(""); // очищаем, перезаписав поверх пустую строку
+            out1.close(); // закрываем
 
-        GameController gameController = GameController.getInstance( new FileInputStream(input_file), new PrintStream(fileOutputStream));
-        gameController.start();
+            FileOutputStream fileOutputStream = new FileOutputStream("out_text.txt", true);
 
-        return "";
+            GameController gameController = GameController.getInstance( new FileInputStream(input_file), new PrintStream(fileOutputStream));
+            gameController.start();
+
+        }
+        catch (Exception ex) {
+            fail();
+        }
+
+        try {
+            Object[] correct = Files.lines(Paths.get("test/" + test_name + "/expected_output.txt"), StandardCharsets.UTF_8).toArray();
+            Object[] real = Files.lines(Paths.get("out_text.txt"), StandardCharsets.UTF_8).toArray();
+
+            int sz = 0;
+            int other_sz = 0;
+
+            for (Object x: correct)  {
+                sz += 1;
+            }
+            for (Object x: real)  {
+                other_sz += 1;
+            }
+
+            if (sz != other_sz) {
+                fail();
+                return;
+            }
+
+            for (int i = 0; i < sz; ++i)  {
+                if (!((String) correct[i]).equals((String)real[i])) {
+                    fail();
+                }
+            }
+        }
+        catch (Exception ignored)
+        {
+            fail();
+        }
     }
 }
