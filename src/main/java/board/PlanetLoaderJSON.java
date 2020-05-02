cb23ada48028e47717f45605b9748e3d5bb7852d
aleks311001 <aleks311001@yandex.ru>
2020-05-02 04:20:46 +0300

    Fix resources

--- src/main/java/board/PlanetLoaderJSON.java
+++ src/main/java/board/PlanetLoaderJSON.java
@@ -3,7 +3,9 @@ package board;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.InputStreamReader;
 
 public interface PlanetLoaderJSON {
     void setAllFromJSON(JSONObject object);
@@ -11,7 +13,8 @@ public interface PlanetLoaderJSON {
         String[] className = getClass().getName().split("\\.");
         String filename = className[className.length - 1] + raceName + ".json";
 
-        try (FileReader reader = new FileReader("etc/baseUnits/" + filename)) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(
+                "/etc/baseUnits/" + filename)))) {
             JSONTokener token = new JSONTokener(reader);
             JSONObject object = new JSONObject(token);
             setAllFromJSON(object);
