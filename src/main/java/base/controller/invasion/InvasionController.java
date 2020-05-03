a7895069f7f404fbdb9e4ede433505826fc030ba
IgorMelnikov1 <melnikov.ia@phystech.edu>
2020-05-03 20:30:59 +0300

    main variables

--- src/main/java/base/controller/invasion/InvasionController.java
+++ src/main/java/base/controller/invasion/InvasionController.java
@@ -7,12 +7,17 @@ import base.controller.CommandResponse;
 import base.controller.global.GlobalCommandController;
 import base.model.GameState;
 import base.user.CommandRequestable;
+import board.Tile;
 import board.TileObject;
 import player.Player;
 
 public class InvasionController extends AbstractController {
+    private Player invader;
+    private Tile tile;
+    private GameState gameState;
+
     protected InvasionController(CommandRequestable userInterface, GameState gameState,
-                                 GlobalCommandController globalCommandController, TileObject space, Player player) {
+                                 GlobalCommandController globalCommandController, Tile tile, Player player) {
         super(userInterface, globalCommandController);
         super.putCommand("bombard", new PlayerInvasionBombard(this));
         super.putCommand("end-bombarding", new PlayerInvasionEndBombarding(this));
@@ -22,7 +27,13 @@ public class InvasionController extends AbstractController {
         super.putCommand("do-not-defend", new PlayerInvasionDoNotDefend(this));
         super.putCommand("assign-hits", new PlayerInvasionAssignHits(this));
         super.putCommand("make-rolls", new PlayerInvasionMakeRoll(this));
+
+        this.invader = player;
+        this.tile = tile;
+        this.gameState = gameState;
     }
+
+
 
     @Override
     public CommandResponse start() {
