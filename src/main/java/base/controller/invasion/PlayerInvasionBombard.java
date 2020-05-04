246a2db04ba45e68ed8ea34fc696db41d392a25a
IgorMelnikov1 <melnikov.ia@phystech.edu>
2020-05-05 00:21:04 +0300

    invasion moddified

--- src/main/java/base/controller/invasion/PlayerInvasionBombard.java
+++ src/main/java/base/controller/invasion/PlayerInvasionBombard.java
@@ -3,20 +3,80 @@ package base.controller.invasion;
 import base.controller.CommandResponse;
 import base.controller.combat.SpaceCombatController;
 import base.user.CommandRequestable;
+import board.Planet;
+import player.Army;
 import player.Player;
+import player.units.GroundForce.GroundForce;
+import player.units.GroundForce.Infantry;
+import player.units.Ships.Ship;
+import player.units.Unit;
+
+import javax.swing.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class PlayerInvasionBombard extends PlayerInvasionCommand  {
+    private ArrayList<Ship.Target> targets;
+    private Planet.Target planet_target;
+
+
     public PlayerInvasionBombard(InvasionController controller) {
         super(controller);
+        this.targets = new ArrayList<Ship.Target>();
+        planet_target = null;
     }
 
+
     @Override
-    public boolean inputCommand(CommandRequestable userInterface) {
+    public boolean inputCommand(CommandRequestable userInterface) { //What should I return?
+        targets.clear();
+
+        int sz = userInterface.requestNumber("Insert number of units");
+
+        for (int i = 0; i < sz; ++i) {
+            Object unit = userInterface.requestTarget("Insert unit");
+            if (unit instanceof GroundForce.Target) {
+                targets.add((Ship.Target)unit);
+            }
+            else {
+                getController().getUserInterface().reportError("Should've got ship target.\n");
+
+            }
+        }
+
+        Object planet = userInterface.requestTarget("Insert planet");
+        if (planet instanceof Planet.Target) {
+            this.planet_target = (Planet.Target)planet;
+        }
+        else {
+            getController().getUserInterface().reportError("Should've got infantry target.\n");
+
+        }
+
         return false;
     }
 
     @Override
     public CommandResponse execute(Player player) {
-        return null;
+        ArrayList<Unit> units = new ArrayList<Unit>();
+        Planet planet = null;
+
+        for (Ship.Target target: targets) {
+            Object soldier = player.getObject(target);
+
+            if (soldier instanceof Ship && ((Ship) soldier).hasBombardment()) {
+                units.add((Ship)soldier);
+            }
+            else {
+                getController().getUserInterface().reportError("Should've got infantry.\n");
+                return CommandResponse.DECLINED;
+            }
+        }
+
+        planet = (Planet) player.getObject(planet_target);
+
+        ((InvasionController)controller).GetBombardmentManager().Add(units, planet);
+
+        return CommandResponse.ACCEPTED;
     }
 }
--- src/main/java/base/controller/invasion/PlayerInvasionLandTroops.java
+++ src/main/java/base/controller/invasion/PlayerInvasionLandTroops.java
@@ -3,20 +3,74 @@ package base.controller.invasion;
 import base.controller.CommandResponse;
 import base.controller.combat.SpaceCombatController;
 import base.user.CommandRequestable;
+import board.Planet;
 import player.Player;
+import player.units.GroundForce.GroundForce;
+import player.units.GroundForce.Infantry;
+import player.units.Unit;
+
+import java.util.ArrayList;
 
 public class PlayerInvasionLandTroops extends PlayerInvasionCommand {
+    private ArrayList<GroundForce.Target> targets;
+    private Planet.Target planet_target;
+
     public PlayerInvasionLandTroops(InvasionController controller) {
         super(controller);
+        this.targets = new ArrayList<GroundForce.Target>();
+        planet_target = null;
     }
 
     @Override
     public boolean inputCommand(CommandRequestable userInterface) {
+        targets.clear();
+
+        int sz = userInterface.requestNumber("Insert number of units to land");
+
+        for (int i = 0; i < sz; ++i) {
+            Object unit = userInterface.requestTarget("Insert unit to land");
+            if (unit instanceof GroundForce.Target) {
+                targets.add((GroundForce.Target)unit);
+            }
+            else {
+                getController().getUserInterface().reportError("Should've got infantry target.\n");
+
+            }
+        }
+
+        Object planet = userInterface.requestTarget("Insert planet");
+        if (planet instanceof Planet.Target) {
+            this.planet_target = (Planet.Target)planet;
+        }
+        else {
+            getController().getUserInterface().reportError("Should've got infantry target.\n");
+
+        }
+
         return false;
     }
 
     @Override
     public CommandResponse execute(Player player) {
-        return null;
+        ArrayList<Unit> units = new ArrayList<Unit>();
+        Planet planet = null;
+
+        for (GroundForce.Target target: targets) {
+            Object soldier = player.getObject(target);
+
+            if (soldier instanceof Infantry) {
+                units.add((Infantry)soldier);
+            }
+            else {
+                getController().getUserInterface().reportError("Should've got infantry to land.\n");
+                return CommandResponse.DECLINED;
+            }
+        }
+
+        planet = (Planet) player.getObject(planet_target);
+
+        ((InvasionController)controller).GetBombardmentManager().Add(units, planet);
+
+        return CommandResponse.ACCEPTED;
     }
 }
