52bd1e43c2f4449b620ed9226595d570f028c005
aleks311001 <aleks311001@yandex.ru>
2020-05-13 00:04:05 +0300

    add roll dices

--- src/main/java/base/controller/invasion/InvasionController.java
+++ src/main/java/base/controller/invasion/InvasionController.java
@@ -6,6 +6,7 @@ import base.controller.CommandResponse;
 
 import base.controller.combat.SpaceCombatController;
 import base.controller.global.GlobalCommandController;
+import base.controller.groundCombat.PlayerSpaceCannon;
 import base.model.GameState;
 import base.user.CommandRequestable;
 import board.Tile;
@@ -31,8 +32,14 @@ public class InvasionController extends AbstractController {
     private Player invader;
     private Tile tile;
     private GameState gameState;
+    private CombatPhase combatPhase;
 
     private BombardmentManager bombardmentManager;
+
+    private Integer numHits;
+    public Integer getNumHits() {
+        return numHits;
+    }
 
     public BombardmentManager GetBombardmentManager () {
         return bombardmentManager;
@@ -42,7 +49,6 @@ public class InvasionController extends AbstractController {
                                  GlobalCommandController globalCommandController, Tile tile, Player player) {
         super(userInterface, globalCommandController);
         super.putCommand("bombard", new PlayerInvasionBombard(this));
-        super.putCommand("end-bombarding", new PlayerInvasionEndBombarding(this));
         super.putCommand("land-troops", new PlayerInvasionLandTroops(this));
         super.putCommand("finish", new PlayerInvasionFinish(this));
         super.putCommand("assign-hits", new PlayerInvasionAssignHits(this));
@@ -62,7 +68,8 @@ public class InvasionController extends AbstractController {
 
     @Override
     public CommandResponse start() {
-        CommandResponse response;
+        AbstractCommand command = new PlayerInvasionBombard(this);
+        CommandResponse response = CommandResponse.DECLINED;
 
 
         return CommandResponse.ACCEPTED;
