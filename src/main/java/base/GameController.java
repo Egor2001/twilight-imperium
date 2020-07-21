cc11712fcfd9d84e216d0c5370421b5a266f6fd0
Egor Elchinov <elchinov.es@phystech.edu>
2020-07-21 04:45:28 +0300

    mvn javadoc added

--- src/main/java/base/GameController.java
+++ src/main/java/base/GameController.java
@@ -16,12 +16,24 @@ import base.model.GameState;
 import java.io.*;
 import java.util.ArrayList;
 
+/**
+ * Root controller
+ * processes main user actions
+ * delegates processing other actions to other responsible controllers
+ * implements singleton pattern
+ * @see AbstractController
+ * @author geome_try
+ */
 public class GameController extends AbstractController {
 
     private static GameController instance;
     //private static Object IOUtils;
     private GameState gameState;
 
+    /**
+     * Singleton getter and default constructor if no instance presents
+     * @return unique class instance
+     */
     public static GameController getInstance() {
         if (instance == null) {
             instance = new GameController(System.in, System.out);
@@ -30,6 +42,12 @@ public class GameController extends AbstractController {
         return instance;
     }
 
+    /**
+     * Singleton getter and constructor if no instance presents
+     * @param inputStream input stream to read user commands from
+     * @param printStream print stream to write game events' info to
+     * @return unique class instance
+     */
     public static GameController getInstance(InputStream inputStream, PrintStream printStream) {
         //BufferedReader reader = new BufferedReader(new FileReader(input_file))
         //new FileReader(input_file);
@@ -47,20 +65,38 @@ public class GameController extends AbstractController {
         this.globalCommandController = new GlobalCommandController(this.userInterface, this.gameState);
     }
 
+    /**
+     * UI object getter
+     * @return user interface object
+     */
     public CommandRequestable getUserInterface() {
         return userInterface;
     }
 
+    /**
+     * Game state object getter
+     * @return game state
+     */
     @Override
     public GameState getGameState() {
         return gameState;
     }
 
+    /**
+     * Exit command builder to satisfy AbstractController interface
+     * @see AbstractCommand
+     * @return exit command for this controller
+     */
     @Override
     public AbstractCommand getExitCommand() {
         return new PlayerGlobalExit(null);
     }
 
+    /**
+     * Controller's entry point function
+     * @see CommandResponse
+     * @return response of exit result
+     */
     @Override
     public CommandResponse start() {
         int playersCnt = gameState.getPlayers().size();
@@ -84,6 +120,12 @@ public class GameController extends AbstractController {
         return CommandResponse.ACCEPTED;
     }
 
+    /**
+     * Requests user to customize its player
+     * @see Player
+     * @param idx user's game order number
+     * @return player class object
+     */
     private Player requestPlayer(int idx) {
         Player player = null;
 
@@ -101,8 +143,12 @@ public class GameController extends AbstractController {
         return player;
     }
 
-    public static void Delete ()
-    {
+    /**
+     * Deletes current instance
+     * Warning! unsafe due to singleton pattern requirements violation
+     * Should be removed in future versions
+     */
+    public static void Delete () {
         instance = null;
     }
 }
--- src/main/java/base/Main.java
+++ src/main/java/base/Main.java
@@ -2,8 +2,18 @@ package base;
 
 import java.util.ArrayList;
 
+/**
+ * The main class
+ * @author geome_try
+ * @see GameController
+ */
+
 public class Main {
 
+    /**
+     * The entry point function
+     * @param args CLI arguments
+     */
     public static void main(String[] args) {
         GameController.getInstance().start();
     }
